414c0412fc67cd689a0ca63f0b10678ce9db4e48
Devarsh <75148774+webdev03@users.noreply.github.com>
2024-12-31 02:40:01 +0000

    Initial code

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
